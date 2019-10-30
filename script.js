2331a43c4106fea3dea33ad3301437733c4ef158
Caleb Smith <casmittie44@gmail.com>
2019-10-30 11:37:16 +1100

    Initial commit of weather application.

---
+++ script.js
@@ -0,0 +1,52 @@
+// Clock example
+class Clock extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state = {date: new Date()};
+    }
+    
+    componentDidMount() {
+	this.timerID = setInterval(
+	    ()=>this.tick(),
+	    1000
+	);
+    }
+    
+    componentWillUnmount() {
+	clearInterval(this.timerID);
+    }
+    
+    tick() {
+	this.setState( {
+	    date: new Date()
+	});
+    }
+    
+    render() {
+	return(
+		<div>
+		<h1>Hello, World!</h1>
+		<h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+		</div>
+	);
+    }
+}
+
+function App() {
+    return(
+	    <div>
+	    <Clock />
+	    <Clock />
+	    <Clock />
+	    </div>
+    );
+}
+
+function kelvinToCelsius(kelvins) {
+    return kelvins - 273.15;
+}
+
+ReactDOM.render(
+	<App />,
+    document.getElementById('root')
+);
