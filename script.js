6d70676a537068f9f61f59a39e94c3650540a714
Caleb Smith <casmittie44@gmail.com>
2019-10-31 11:14:56 +1100

    App now displays the current temperature in Melbourne, aus. It makes use of a Weather component which waits for a response from openweather.api. Before the response is received, it displays a loading message. After the response is received, it displays the current temperature in Centigrade.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 // Clock example
 class Clock extends React.Component {
     constructor(props) {
@@ -42,8 +43,86 @@ function App() {
     );
 }
 
-function kelvinToCelsius(kelvins) {
-    return kelvins - 273.15;
+/* Inline script version    
+var request = new XMLHttpRequest();
+request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1691f032793d309e08203112cd0cd698', true);
+
+//request.open('GET', 'https://ghibliapi.herokuapp.com/films',true);
+request.addEventListener("load", output);
+
+function output() {
+      var data = JSON.parse(this.response);
+      console.log("It worked!");
+      ReactDOM.render(
+      <div>
+	<h1>It worked!</h1>
+	<p>{data['main']['temp']}</p>
+      </div>,
+	document.getElementById('root')
+	);
+};
+*/
+
+function App() {
+    return(
+	    <div>
+	    <Weather />
+	    </div>
+    );
+}
+
+class Weather extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state = {dataReceived: false, temp: 0};
+	this.city = 'Melbourne';
+	this.country = 'aus';
+	this.getWeather(this.city, this.country);
+	//this.processResponse = this.processResponse.bind(this);
+    }
+
+    getWeather(city, country) {
+	var request = new XMLHttpRequest();
+	request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q='
+		     + this.city + ',' + this.country + '&APPID=1691f032793d309e08203112cd0cd698', true);
+	request.onload = () =>
+	{
+	  //  console.log(request.response);
+	    var data = JSON.parse(request.response);
+	    var temperature =  this.kelvinToCelsius(data['main']['temp']);
+	    this.setState({dataReceived: true,
+			   temp: temperature}
+			 );
+
+	    // console.log(this.state.temp);
+	}
+	
+	request.send();
+    }
+
+    kelvinToCelsius(kelvins) {
+	return kelvins - 273.15;
+    }
+    
+    render() {
+	if(this.state.dataReceived) {
+	return (
+		<div>
+		<h1>It worked!</h1>
+		<p>Temperature is {this.state.temp}.</p>
+		</div>
+	);
+	}
+
+	else {
+	    return (
+		    <div>
+		    <h1>It worked!</h1>
+		    <p>Temperature is loading...</p>
+		    </div>
+	    );
+	}
+    }
 }
 
 ReactDOM.render(
